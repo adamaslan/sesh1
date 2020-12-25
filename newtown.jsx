0a5b24e8313ba913ae57d4869a76c57d8b32a06c
Adam Aslan <chillcoders@gmail.com>
2020-12-25 18:32:18 -0500

    whitespace

--- newtown.jsx
+++ newtown.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-
 const NewtApp = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
